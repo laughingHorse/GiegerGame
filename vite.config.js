9b34e7d1e28661b089b198e7aaec5d45aa934eb5
laughingHorse <sibleyaaa@hotmail.com>
2025-08-22 12:47:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  base: '/GiegerGame/' // <-- replace with your repo name exactly
+  base: '/GiegerGame/'   // <-- must match your repo name exactly (case-sensitive)
 });
